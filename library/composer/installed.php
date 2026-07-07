1f0132ee73eaf4f3780fae1ff745c88e616f1fcb
Rochmady <srochmady@gmail.com>
2026-07-07 17:01:53 +0700

    Update reference version in installed.php to match latest commit

--- library/composer/installed.php
+++ library/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'lumera/geoip-lite',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'fb8d9634acaeb1029de623c75f82597b4a7e4574',
+        'reference' => '561d3d91dd2460ed16cf3bd693f1bbbc43bcbdd3',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'lumera/geoip-lite' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'fb8d9634acaeb1029de623c75f82597b4a7e4574',
+            'reference' => '561d3d91dd2460ed16cf3bd693f1bbbc43bcbdd3',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
